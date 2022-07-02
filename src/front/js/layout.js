fef46f25da01fce3b47991d01b404733ebf448e8
Al <alpalma95@gmail.com>
2022-07-02 10:41:13 +0200

    cleaned up boilerplate

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -3,8 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -22,10 +20,7 @@ const Layout = () => {
                 <ScrollToTop>
                     <Navbar />
                     <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<h1>Not found!</h1>} />
+                        <Route element={<Home />} path="/" />                
                     </Routes>
                     <Footer />
                 </ScrollToTop>
