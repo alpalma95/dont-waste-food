8b6aabfc4ca27e13f5c99f84472e5cc596fda7cb
Al <alpalma95@gmail.com>
2022-07-05 13:54:47 +0000

    fixed import errors and updated footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+  <footer className="footer mt-auto py-3 text-center">
+    <p>
+      Made with <i className="fa fa-heart text-danger" /> by{" "}
+      <a href="https://github.com/Jules-11">@Jules-11</a>,{" "}
+      <a href="https://github.com/sardinhas12">@sardinhas12</a> and{" "}
+      <a href="https://github.com/alpalma95">@alpalma95</a>
+    </p>
+  </footer>
 );
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,14 +1,13 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Don't waste my food!</h1>
-		</div>
-	);
+  return (
+    <div className="text-center mt-5">
+      <h1>Don't waste my food!</h1>
+    </div>
+  );
 };
