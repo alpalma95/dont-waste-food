921355caf2410e717083f3af8e94a7e1df27a539
Al <alpalma95@gmail.com>
2022-07-05 18:23:35 +0000

    werkzeug and jwt installed

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,10 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +20,4 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
