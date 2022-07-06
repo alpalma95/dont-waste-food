ba763f3105ae0494ae4125625136862660511bd8
Al <alpalma95@gmail.com>
2022-07-06 09:17:14 +0000

    deleted example endpoint

--- src/api/routes.py
+++ src/api/routes.py
@@ -12,15 +12,6 @@ from flask_jwt_extended import jwt_required
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
-    return jsonify(response_body), 200
-
 @api.route('/signup', methods=['POST'])
 def handle_signup():
     response_body = request.get_json(force=True)
