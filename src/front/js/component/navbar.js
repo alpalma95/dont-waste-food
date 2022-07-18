0ee8ddb85886123f2b03df5105c8d8f0f552e20e
Al <alpalma95@gmail.com>
2022-07-18 11:28:11 +0000

    collapsing navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -16,7 +16,6 @@ export const Navbar = () => {
     actions.getToken();
   };
 
-  console.log("NEW INGREDIENTS", store.shoppingList);
   let list = store.shoppingList.map((ingredient, index) => (
     <li
       className="list-group-item p-0 d-flex flex-row align-items-center justify-content-between"
@@ -35,81 +34,115 @@ export const Navbar = () => {
 
   return (
     <nav
-      className="navbar navbar-light mb-3 p-3 sticky-top d-flex justify-content-end"
+      className="navbar navbar-expand-md navbar-light p-3 sticky-top d-flex justify-content-end"
       style={{ backgroundColor: "#009688" }}
     >
-      <Link to="/" className="me-auto" style={{ textDecoration: "none" }}>
-        <h1 className="navbar-brand" style={{ color: "white" }} href="#">
-          Don't Waste My Food
-        </h1>
-      </Link>
-      <Link to="/login">
-        {store.userToken ? (
-          <></>
-        ) : (
-          <button className="btn border-white m-1 text-white">Login</button>
-        )}
-      </Link>
-      <Link to="/search">
-        {!store.userToken ? (
-          <button className="btn btn-danger border-white m-1 text-white">
-            Search recipes
-          </button>
-        ) : (
-          <button className="btn border-white m-1 text-white">
-            Search recipes
-          </button>
-        )}
-      </Link>
-      <Link to="/">
-        {store.userToken ? (
-          <button
-            className="btn btn-outline-dark m-1"
-            onClick={removeTokenHandler}
-          >
-            Log out
-          </button>
-        ) : (
-          <></>
-        )}
-      </Link>
-      <Link to="/signup">
-        {store.userToken ? (
-          <></>
-        ) : (
-          <button className="btn border-white m-1 text-white">Sign up</button>
-        )}
-      </Link>
-      <div className="ml-auto">
-        <div className="dropdown">
-          <button
-            className="btn btn-dark dropdown-toggle"
-            type="button"
-            id="dropdownMenuButton1"
-            data-bs-toggle="dropdown"
-            data-bs-auto-close="outside"
-            aria-expanded="true"
-            style={{ marginRight: "40px", boxShadow: "none" }}
-          >
-            Shopping List
-            <span className="badge text-bg-secondary">
-              {store.shoppingList.length}
-            </span>
-          </button>
-
-          <ul
-            className="list-group-flush dropdown-menu dropdown-menu-right p-4"
-            aria-labelledby="dropdownMenuButton1"
-          >
-            {list}
-
-            <Link to="/shoppinglist" style={{ textDecoration: "none" }}>
-              <li className="d-flex justify-content-center">
-                {store.shoppingList.length !== 0 && (
-                  <span className="btn btn-dark mt-3">Shopping list</span>
+      <div className="container-fluid">
+        <Link to="/" className="me-auto" style={{ textDecoration: "none" }}>
+          <h1 className="navbar-brand" style={{ color: "white" }} href="#">
+            Don't Waste My Food
+          </h1>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon text-white"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-lg-0">
+            <li className="text-end m-1">
+              <Link to="/login">
+                {store.userToken ? (
+                  <></>
+                ) : (
+                  <button
+                    className="btn border-white m-1 text-white w-100"
+                    data-bs-toggle="collapse"
+                    data-bs-target=".navbar-collapse.show"
+                  >
+                    Login
+                  </button>
                 )}
-              </li>
-            </Link>
+              </Link>
+            </li>
+            <li className="text-end m-1">
+              <Link to="/search">
+                {!store.userToken ? (
+                  <button className="btn bg-danger border-white m-1 text-white w-100" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
+                    Search recipes
+                  </button>
+                ) : (
+                  <button className="btn border-white m-1 text-white w-100" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
+                    Search recipes
+                  </button>
+                )}
+              </Link>
+            </li>
+            <li className="text-end m-1">
+              <Link to="/">
+                {store.userToken ? (
+                  <button
+                    className="btn btn-outline-dark m-1 w-100"
+                    onClick={removeTokenHandler}
+                    data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show"
+                  >
+                    Log out
+                  </button>
+                ) : (
+                  <></>
+                )}
+              </Link>
+            </li>
+            <li className="text-end m-1">
+              <Link to="/signup">
+                {store.userToken ? (
+                  <></>
+                ) : (
+                  <button className="btn border-white m-1 text-white w-100" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
+                    Sign up
+                  </button>
+                )}
+              </Link>
+            </li>
+            <li className="text-end m-1">
+              <div className="dropdown">
+                <button
+                  className="btn btn-dark dropdown-toggle m-1 w-100"
+                  type="button"
+                  id="dropdownMenuButton1"
+                  data-bs-toggle="dropdown"
+                  data-bs-auto-close="outside"
+                  aria-expanded="true"
+                  style={{ marginRight: "40px", boxShadow: "none" }}
+                >
+                  Shopping List
+                  <span className="badge text-bg-secondary">
+                    {store.shoppingList.length}
+                  </span>
+                </button>
+
+                <ul
+                  className="list-group-flush dropdown-menu dropdown-menu-right p-4"
+                  aria-labelledby="dropdownMenuButton1"
+                >
+                  {list}
+
+                  <Link to="/shoppinglist" style={{ textDecoration: "none" }}>
+                    <li className="d-flex justify-content-center">
+                      {store.shoppingList.length !== 0 && (
+                        <span className="btn btn-dark mt-3" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Shopping list</span>
+                      )}
+                    </li>
+                  </Link>
+                </ul>
+              </div>
+            </li>
           </ul>
         </div>
       </div>
