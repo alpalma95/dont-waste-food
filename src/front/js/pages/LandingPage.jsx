2e439dfaa7340639ca96bf8cc134226fbd2ea927
Al <alpalma95@gmail.com>
2022-07-20 07:14:18 +0000

    fixed imports

--- src/front/js/pages/LandingPage.jsx
+++ src/front/js/pages/LandingPage.jsx
@@ -1,10 +1,52 @@
 import React from "react";
+import { render } from "react-dom";
+import tableImage from "../../../img/table.jpg";
 
 const LandingPage = () => {
+  const styles = {
+    backgroundImage: `url(${tableImage})`,
+    backgroundSize: "cover",
+    backgroundPosition: "top",
+  };
+
   return (
-    <div className="container-fluid d-flex justify-content-center pt-5">
-      <h1>I'm the landing page!</h1>
-      <p>I should appear in the Home route if the user is not logged in</p>
+    <div className="image" style={styles}>
+      <div className="box">
+        <h1>The fridge is where magic happens</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm">
+              <div className="text">
+                Your Ingredients
+                <span className="description">
+                  {" "}
+                  Use ingredients you already have on hand
+                </span>
+              </div>
+            </div>
+            <div className="col-sm">
+              <div className="text">
+                Find Recipes
+                <span className="description">
+                  {" "}
+                  Find and select the recipes you want to try. Save them to your
+                  library
+                </span>
+              </div>
+            </div>
+            <div className="col-sm">
+              <div className="text">
+                Enjoy your meal
+                <span className="description">
+                  {" "}
+                  Discover the recipes to try new dishes and make your family
+                  delicious meals
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,22 +4,8 @@ import LandingPage from "./LandingPage.jsx";
 import Dashboard from "./Dashboard.jsx";
 import { Context } from "../store/appContext";
 
-<<<<<<< HEAD
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
-  return (
-    <div className="m-5">
-      {store.userToken ? <Dashboard /> : <LandingPage />}
-    </div>
-  );
+  return <div>{store.userToken ? <Dashboard /> : <LandingPage />}</div>;
 };
-=======
-
-export const Home = () => (
-  <div>
-    <SearchBar />
-    <Recipes />
-  </div>
-);
->>>>>>> origin/patricia
