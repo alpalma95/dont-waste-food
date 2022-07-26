fc9e7e5cc1cce9bed6d0222b56b67f520a99f8f7
Al <alpalma95@gmail.com>
2022-07-26 13:00:17 +0000

    modal displays if user tries to add recipe not logged in

--- src/front/js/component/Modal.jsx
+++ src/front/js/component/Modal.jsx
@@ -1,16 +1,39 @@
 import React, { useState, useContext } from "react";
 import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
 import "../../styles/modal.css";
 
 const Modal = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div className={`modal--custom ${store.showModal ? `block` : `hidden`}`}>
-      <span className="modal__close-btn" onClick={actions.showModalHandler}>
-        ❌
-      </span>
-      <h3>Sign up or login for adding favorites!</h3>
+    <div
+      className={`overlay-custom ${store.showModal ? `grid-custom` : `hidden`}`}
+    >
+      <div className="modal-custom">
+        <span className="modal__close-btn" onClick={actions.showModalHandler}>
+          ❌
+        </span>
+        <h3 className="modal-custom__h3">
+          Sign up or login for adding favorites!
+        </h3>
+        <div className="modal-custom__btns">
+          <Link
+            onClick={actions.showModalHandler}
+            to="/login"
+            className="btn-custom"
+          >
+            Login
+          </Link>
+          <Link
+            onClick={actions.showModalHandler}
+            to="/signup"
+            className="btn-custom"
+          >
+            Signup
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
